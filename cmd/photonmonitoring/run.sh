fe3848c61f79cbb7831cb08597abda8ca626a932
bai <nkbai@163.com>
2018-11-09 09:40:01 +0800

    add example run script

---
+++ cmd/photonmonitoring/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./photonmonitoring --address 0x1a9ec3b0b807464e6d3398a59d6b0a369bf422fa --eth-rpc-endpoint http://127.0.0.1:8545 --keystore-path ~/privnet3/keystore  --password-file "123" --smt 0xd18E3ae61C9277454AaEbdc4733A6b614a24B7dD  --verbosity 5 --debug --registry-contract-address    0xDe661C5aDaF15c243475C5c6BA96634983821593 --photon-url     "http://127.0.0.1:5002/api/1/queryreceivedtransfer"
+
+
+
+
